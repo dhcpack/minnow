32f4f1adc1e2a12971f9247ad2007164433b0ef6
dhcpack <1928521033@qq.com>
2023-04-21 12:03:17 +0000

    :alembic:Translating between 64-bit indexes and 32-bit seqnos

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -5,15 +5,20 @@ using namespace std;
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
   // Your code here.
-  (void)n;
-  (void)zero_point;
-  return Wrap32 { 0 };
+  return zero_point + static_cast<uint32_t>( n );
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
   // Your code here.
-  (void)zero_point;
-  (void)checkpoint;
-  return {};
+  uint32_t offset = this->raw_value_ - wrap( checkpoint, zero_point ).raw_value_;
+  uint64_t res = checkpoint + offset;
+  /*
+    如果offset大于1<<32的一半，则离checkpoint更近的应该是checkpoint前面的元素 res向前一段(2 ^ 32)；
+    需要注意的是如果res小于2^32，则不能向前。
+  */
+  if ( offset >= ( 1U << 31 ) && res >= 1UL << 32 ) {
+    res -= ( 1UL << 32 );
+  }
+  return res;
 }
